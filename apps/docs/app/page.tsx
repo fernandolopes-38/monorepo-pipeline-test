00d4accb9193c35607621914653b3fd182a365df
Lopes Fernando <fep@portugal.conseur.org>
2026-08-26 01:26:44 +0100

    teste deploy

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -36,7 +36,8 @@ export default function Home() {
             Get started by editing <code>apps/docs/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>ENV: {process.env.ENVIROMENT}!!!</li>
+          <li>Testando deploy automático</li>
+          <li>ENV: {process.env.NEXT_PUBLIC_ENVIROMENT}!!!</li>
         </ol>
 
         <div className={styles.ctas}>
